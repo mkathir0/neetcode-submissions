0816be2ed6076c19bbc670d68102d655f55a6a09
Mithrajit Kathir <mithrajit07@gmail.com>
2026-08-05 15:34:03 +0530

    Add: is-anagram - submission-17

---
+++ Data Structures & Algorithms/is-anagram/submission-17.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hmap1={}
+
+        for i in s:
+            hmap1[i]=hmap1.get(i,0)+1
+        
+        for i in t:
+            hmap1[i]=hmap1.get(i,0)-1
+
+        for i in hmap1.values():
+            if i!=0:
+                return False
+        
+        return True
